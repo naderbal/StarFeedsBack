6afecbbf440b0d2526c58064e8694fba1c653372
Ali Kaddoura <ali.kaddoura@live.com>
2017-05-26 22:11:19 +0300

    logo change

--- resources/views/pages/following.blade.php
+++ resources/views/pages/following.blade.php
@@ -12,7 +12,7 @@
             @foreach($following as $result)
 
                 <div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 col-lg-pull-1 col-md-pull-1 col-md-offset-1 col-sm-offset-1 col-sm-pull-2 col-xs-pull-2 col-xs-offset-2" style="margin-bottom: 5px;">
-                    <div class="card secondary" style="border: 1px solid #ccc;border-radius: 5px;width: 150%;overflow: hidden;margin-bottom: 10px;">
+                    <div class="card secondary">
                         <div style="margin:5px;">
                             <img class="card-img-top img-circle center-block img-responsive" style="width:80px;  max-height:150px;" src="{{ $result->fb_profile_url }}" alt="Card image cap">
                             <div class="card-block">
